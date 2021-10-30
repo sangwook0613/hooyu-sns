3d4d6ec48535754130be4fab8fd3878d454b651a
qlfflwls5 <qlfflwls2@gmail.com>
2021-10-31 00:47:37 +0900

    S05P31A101-68 feat: Add a mainpage add button

--- frontend/src/screen/Main.js
+++ frontend/src/screen/Main.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Dimensions } from 'react-native';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image, TouchableOpacity } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import AddButton from '../assets/images/add.png'
 
 
 const device_width = Dimensions.get('window').width
 const device_height = Dimensions.get('window').height
+const mainColor1 = '#A1D1E7'
+const mainColor2 = '#71D2FF'
+const mainColor3 = '#FDA604'
 
 function Main() {
 
@@ -23,39 +27,63 @@ function Main() {
           
         </View>
         <View style={{flexDirection: "row", marginTop: 10}}>
-          <Text style={{marginRight: 20, transform: [{ rotate: '30deg'}]}}>20m</Text>
+          <Text style={{top: -20, marginRight: 20, transform: [{ rotate: '30deg'}]}}>20m</Text>
           <Text style={{marginRight: 20, transform: [{ rotate: '10deg'}]}}>100m</Text>
           <Text style={{marginRight: 20, transform: [{ rotate: '-10deg'}]}}>500m</Text>
-          <Text style={{transform: [{ rotate: '-30deg'}]}}>2km</Text>
+          <Text style={{top: -20, transform: [{ rotate: '-30deg'}]}}>2km</Text>
         </View>
+      </View>
+      <View
+        style={styles.addButtonContainer}
+      >
+        <TouchableOpacity style={styles.addButton}>
+          <Image 
+            style={styles.addButtonImage}
+            source={AddButton}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
       </View>
     </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
+  addButton: {
+    alignItems: 'center',
+    backgroundColor: mainColor3,
+    borderRadius: 35,
+    elevation: 4,
+    height: device_width * 0.12,
+    justifyContent: 'center',
+    marginTop: device_height * 0.13,
+    width: device_width * 0.12,
+  },
+  addButtonContainer: {
+    alignItems: 'center',
+    flex: 0.25,
+  },
+  addButtonImage: {
+    width: device_width * 0.06,
+  },
   linearGradient: {
     flex: 1,
-  },
-  rader: {
-    borderRadius: Math.round(device_width + device_height) / 2,
-    width: device_width * 0.7,
-    height: device_width * 0.7,
-    borderColor: "#FDA604",
-    borderWidth: 5,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
   profileButton: {
     flex:0.05,
     alignItems: "flex-end",
   },
-
-  button20m: {
-  }
-
-
+  rader: {
+    borderRadius: Math.round(device_width + device_height) / 2,
+    width: device_width * 0.7,
+    height: device_width * 0.7,
+    borderColor: mainColor3,
+    borderWidth: 5,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4,
+  },
 });
 
 
