f4e4a26d55b33a75ae34e77498d81aa8e59b4246
ryu <munam4@naver.com>
2021-10-31 03:54:27 +0900

    S05P31A101-69 feat: Add mainpage layout (profilebutton, shelter, background)

--- frontend/src/screen/Main.js
+++ frontend/src/screen/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Dimensions } from 'react-native';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-
+import shelter from '../assets/images/shelter.png'
+import wowImoticon from '../assets/images/wowimoticon.png'
+import cloud1 from '../assets/images/cloud1.png'
 
 const device_width = Dimensions.get('window').width
 const device_height = Dimensions.get('window').height
@@ -11,22 +13,39 @@ function Main() {
 
   return (
     <LinearGradient colors={['#A1D1E7', '#CDE4EE']} style={styles.linearGradient}>
+      <Image source={cloud1} style={styles.cloud1} resizeMode="contain" />
+      <Image source={cloud1} style={styles.cloud2} resizeMode="contain" />
       <View style={styles.profileButton}>
-        <Text >프로필</Text>
+        <TouchableOpacity>
+          <View>
+            <View style={styles.profileBackground}></View>
+            <Image
+              source={wowImoticon}
+              style={styles.profileImoticon}
+            />
+            <View style={styles.profileMeArea}>
+              <Text style={styles.profileMeText}>me</Text>
+            </View>
+          </View>
+
+        </TouchableOpacity>
       </View>
-      <View style={{
-        flex: 0.7,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <View style={styles.rader}>
-          
+
+      <View style={styles.raderArea}>
+        <View style={styles.shelterArea}>
+          <Image
+            source={shelter}
+            style={styles.shelterImage}
+          ></Image>
         </View>
-        <View style={{flexDirection: "row", marginTop: 10}}>
-          <Text style={{marginRight: 20, transform: [{ rotate: '30deg'}]}}>20m</Text>
-          <Text style={{marginRight: 20, transform: [{ rotate: '10deg'}]}}>100m</Text>
-          <Text style={{marginRight: 20, transform: [{ rotate: '-10deg'}]}}>500m</Text>
-          <Text style={{transform: [{ rotate: '-30deg'}]}}>2km</Text>
+        <View style={styles.rader}>
+
+        </View>
+        <View style={styles.scopes}>
+          <Text style={{ marginRight: 20, transform: [{ rotate: '30deg' }] }}>20m</Text>
+          <Text style={{ marginRight: 20, transform: [{ rotate: '10deg' }] }}>100m</Text>
+          <Text style={{ marginRight: 20, transform: [{ rotate: '-10deg' }] }}>500m</Text>
+          <Text style={{ transform: [{ rotate: '-30deg' }] }}>2km</Text>
         </View>
       </View>
     </LinearGradient>
@@ -34,8 +53,66 @@ function Main() {
 }
 
 const styles = StyleSheet.create({
+  cloud1: {
+    position: "absolute",
+    width: device_width * 0.3
+  },
+  cloud2: {
+    bottom: 30,
+    right: 30,
+    position: "absolute",
+    width: device_width * 0.4
+  },
   linearGradient: {
     flex: 1,
+  },
+  profileButton: {
+    flex: 0.05,
+    alignItems: "flex-end",
+  },
+  profileBackground: {
+    position: "absolute",
+    backgroundColor: 'white',
+    borderRadius: Math.round(device_width + device_height) / 2,
+    top: device_height * 0.02 - 2,
+    right: device_width * 0.03 - 2.5,
+    width: 45,
+    height: 45,
+    // width: device_width * 0.11,
+    // height: device_width * 0.11,
+  },
+  profileImoticon: {
+    marginTop: device_height * 0.02,
+    marginRight: device_width * 0.03,
+    width: 40,
+    height: 40,
+    // width: device_width * 0.1,
+    // height: device_width * 0.1
+  },
+  profileMeArea: {
+    backgroundColor: 'white',
+    borderRadius: Math.round(device_width + device_height) / 2,
+    borderColor: "black",
+    borderWidth: 1.5,
+    position: "absolute",
+    top: device_height * 0.02 + 25,
+    right: device_width * 0.03 - 7,
+    width: 20,
+    height: 20,
+    // width: device_width * 0.05,
+    // height: device_width * 0.05,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  profileMeText: {
+    fontSize: 10,
+    marginBottom: 2
+  },
+  raderArea: {
+    flex: 0.7,
+    justifyContent: 'center',
+    alignItems: 'center',
+
   },
   rader: {
     borderRadius: Math.round(device_width + device_height) / 2,
@@ -47,13 +124,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  profileButton: {
-    flex:0.05,
-    alignItems: "flex-end",
+  shelterArea: {
+    alignItems: 'flex-end',
+    position: 'absolute',
+    top: device_height * 0.12,
+    width: device_width * 0.8
   },
-
-  button20m: {
+  shelterImage: {
+    width: device_width * 0.08,
+    height: device_width * 0.08
+  },
+  scopes: {
+    flexDirection: "row",
+    marginTop: 10
   }
+
+
 
 
 });
