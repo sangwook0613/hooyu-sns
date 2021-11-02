7b752a3601f446445623faa68d802b3cf66b6b8c
ty1295 <ty1295@naver.com>
2021-11-03 00:59:49 +0900

    S05P31A101-122 feat: redux setting

--- frontend/App.js
+++ frontend/App.js
@@ -3,14 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 // import Main from './src/screen/Main';
 import Root from './src/navigation/Root';
+import { Provider } from 'react-redux';
+import Store from './src/store/store';
 
+const store = Store()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Root />
-      {/* <Main/> */}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Root />
+        {/* <Main/> */}
+      </NavigationContainer>
+    </Provider>
   );
 }
 
