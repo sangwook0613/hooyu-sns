aa7d21487e41646f3d6e22e8715e4635501f0fe9
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-11-03 11:27:54 +0900

    S05P31A101-113 Feat: Create statusCreate layout

---
+++ frontend/src/screen/contentCreate/Status.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+const Status = ({ navigation: { navigate }}) => {
+  
+  
+  
+  
+  return (
+    <View>
+      <Text>Status Create Screen</Text>
+      
+    </View>
+  )
+}
+
+export default Status;
