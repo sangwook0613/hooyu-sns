ebbf9f508966d5a6bd8cb99fe8ff56128d310d6f
sangwook0613 <kvmoke@gmail.com>
2021-11-04 01:41:30 +0900

    S05P31A101-121 feat: profile screen layout setting #2

--- frontend/src/screen/ProfileScreen.jsx
+++ frontend/src/screen/ProfileScreen.jsx
@@ -23,29 +23,21 @@ const deviceHeight = Dimensions.get('window').height
 
 const ProfileScreen = ({ navigation, route }) => {
   const [isStatus, setIsStatus] = useState(true)
-  const [isImage, setIsImage] = useState(false)
-  const [isSurvey, setIsSurvey] = useState(false)
-  const flatListRef = useRef()
+  const [isImage, setIsImage] = useState(true)
+  const [isSurvey, setIsSurvey] = useState(true)
+  const scrollRef = useRef()
   
   useEffect(() => {
     setTimeout(() => {
-        const node = flatListRef.current;
-        node.scrollToOffset({ offset: 500, animated: true });
+        const node = scrollRef.current;
+        node.scrollTo({ y: 500, animated: true });
     }, 400);
   },[]);
-  
-  const moveScroll = () => {
-    flatListRef.current.scrollToOffset({
-      offset: 100,
-      Animation: true
-    })
-  }
-
 
   const ProfileTitle = () => {
     return (
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <TouchableOpacity onPress={() => {console.log('EMOJI!!')}}>
+        <TouchableOpacity onPress={() => { console.log('EMOJI!!') }}>
           <Image
             style={{ width: 50, height: 50 }}
             source={route.params.emoji}
@@ -67,81 +59,12 @@ const ProfileScreen = ({ navigation, route }) => {
     });
   }, [navigation]);
 
-  const contentTitle = () => {
-    return (
-      <View
-        style={{
-          width: deviceWidth,
-          height: 50,
-          backgroundColor: 'white',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingLeft: 20,
-          paddingRight: 20,
-          alignItems: 'center'
-        }}
-      >
-        <Text>상태창</Text>
-        <TouchableOpacity onPress={() => {
-          flatListRef.current.scrollToOffset({
-            offset: 100,
-            Animation: true
-          })
-        }}>
-          <AntDesign name="exclamationcircleo" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
-    )
-  }
-
-
-  const renderImageContent = () => {
-    return (
-      <>
-        <View
-          style={{
-            width: deviceWidth,
-            height: 50,
-            backgroundColor: 'white',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            paddingLeft: 20,
-            paddingRight: 20,
-            alignItems: 'center'
-          }}
-        >
-          <Text>이미지</Text>
-          <TouchableOpacity onPress={() => {
-            flatListRef.current.scrollToOffset({
-              offset: deviceHeight,
-              Animation: true
-            })
-          }}>
-            <AntDesign name="exclamationcircleo" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-        <FlatList
-          style={{ marginBottom: 20 }}
-          data={dummyData2}
-          keyExtractor={(item) => item.id + ""}
-          renderItem={() => (
-            <ImageContent />
-          )}
-        />
-      </>
-    )
-  };
 
   return (
-    <FlatList
-      ref={flatListRef}
-      ListFooterComponent={
-        // contentTitle
-        renderImageContent
-      }
-      data={dummyData}
-      keyExtractor={(item) => item.id + ""}
-      renderItem={() => (
+    <ScrollView
+      ref={scrollRef}
+    >
+      {isStatus && (
         <>
           <View
             style={{
@@ -157,8 +80,8 @@ const ProfileScreen = ({ navigation, route }) => {
           >
             <Text>상태창</Text>
             <TouchableOpacity onPress={() => {
-              ref.current.scrollToOffset({
-                offset: deviceHeight,
+              flatListRef.current.scrollToOffset({
+                offset: 100,
                 Animation: true
               })
             }}>
@@ -168,8 +91,35 @@ const ProfileScreen = ({ navigation, route }) => {
           <StatusContent />
         </>
       )}
-    >
-    </FlatList>
+
+      {isImage && (
+        <>
+          <View
+            style={{
+              width: deviceWidth,
+              height: 50,
+              backgroundColor: 'white',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingLeft: 20,
+              paddingRight: 20,
+              alignItems: 'center'
+            }}
+          >
+            <Text>이미지</Text>
+            <TouchableOpacity onPress={() => {
+              flatListRef.current.scrollToOffset({
+                offset: deviceHeight,
+                Animation: true
+              })
+            }}>
+              <AntDesign name="exclamationcircleo" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <ImageContent />
+        </>
+      )}
+    </ScrollView>
   )
 }
 
