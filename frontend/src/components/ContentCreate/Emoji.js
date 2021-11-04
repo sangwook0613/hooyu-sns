36e6896754d69bad480afc577cacaa1fae5d5cdf
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-11-04 23:33:49 +0900

    S05P31A101-110 Feat: Finish Content Create  Layout

--- frontend/src/components/ContentCreate/Emoji.js
+++ frontend/src/components/ContentCreate/Emoji.js
@@ -1,14 +1,16 @@
 import React, {useRef, useState} from 'react';
 import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions, TextInput, Image } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-
-const clientWidth = Dimensions.get('screen').width
-const clientHeight = Dimensions.get('screen').height
-
-const colorArray = ['#FFD0D0', 'red', 'orange', 'yellow', 'green', 'blue', 'purple']
+const emojiArray = [
+  ['amazing', 'amazing', 'amazing', 'amazing', 'amazing', 'amazing'], 
+  ['amazing', 'amazing', 'amazing', 'amazing', 'amazing', 'amazing']
+]
 
 const Emoji = ({ navigation, route }) => {
   
+  const [isEmojiSelect, setIsEmojiSelect] = useState(false)
+
   const EmojiTitle = () => {
     return (
       <View>
@@ -29,80 +31,40 @@ const Emoji = ({ navigation, route }) => {
   }, [navigation]);
 
 
-  const [color, setColor] = useState('#FFD0D0')
-  const [colorScrollX, setColorScrollX] = useState(0)
-  const statusBackground = useRef()
-  const colorScroll = useRef()
-
-  const onEndScroll = () => {
-    colorScroll.current.scrollTo({ x : parseInt((colorScrollX+35)/70)*70 })
-  }
-
-  const onColorPress = (index) => {
-    colorScroll.current.scrollTo({ x : index*70 })
-    // setIsBlack('white')
-  }
-
-  const backgroundStyle = (color) => {
-    return {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      backgroundColor: color,
-    }
-  }
-
   return (
-    <View style={backgroundStyle(color)} ref={statusBackground}>
-      <View style={styles.foo}>
-        <Text></Text>
-      </View>
-      <View style={styles.statusBox}>
-        <TextInput 
-          placeholder={"이모지를 입력해주세요"}
+    <LinearGradient colors={["#AB79EF", "#FC98AB"]} style={styles.mainView}>
+      <View style={styles.emojiSelect}>
+        <TouchableOpacity
+          onPress={() => {setIsEmojiSelect(!isEmojiSelect)}}
+        >
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={route.params.emoji}
           />
+        </TouchableOpacity>
       </View>
-      <View style={styles.scrollViewBox} >
-        <View style={styles.scrollViewInner}>
-          <ScrollView 
-            style={styles.scrollView}
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            onScroll = {(e) => {
-              setColorScrollX(e.nativeEvent.contentOffset.x)
-              setColor(colorArray[parseInt((colorScrollX+35)/70)])
-            }}
-            onScrollEndDrag={onEndScroll}
-            ref={colorScroll}
-          >
-            <View style={styles.blankBox}></View>
-            {
-              colorArray.map((color, index) => (
-                <TouchableOpacity 
-                onPress={() => {onColorPress(index)}} 
-                key={index}
-                >
-                  <View
-                    style={{ 
-                      width: 40,
-                      height: 40,
-                      backgroundColor: color,
-                      borderColor: colorArray[parseInt((colorScrollX+35)/70)] === color ? 'black':'white' ,
-                      borderWidth: 2,
-                      marginHorizontal: 15
-                    }}
-                  ></View>
+      <View style={isEmojiSelect ? styles.emojiSelectBoxClose: styles.emojiSelectBoxOpen}>
+        { [0, 1].map((num, index) => (
+          <View key={index} style={styles.emojiSelectRow}>
+            {emojiArray[num].map((emotion, index2) => (
+              <View key={index2} style={styles.emojiSelectCol}>
+                <TouchableOpacity
+                  onPress={() => {
+                    setIsEmojiSelect(false)
+                    console.warn(index, index2)
+                  }}
+                  >
+                  <Image 
+                    source={route.params.emoji}
+                    style={{width: '100%', height: '100%'}}
+                    />
                 </TouchableOpacity>
-                
-              ))
-            }
-            <View style={styles.blankBox}></View>
-
-          </ScrollView>
-        </View>
+              </View>
+              ))}
+          </View>
+        ))}
       </View>
-    </View>
+    </LinearGradient>
   )
 }
 
@@ -111,39 +73,52 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: "#FFD0D0",
+    justifyContent: 'center',
   },
-  foo: {
-    flex: 1,
+  emojiSelect: {
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  statusBox: {
-    flex: 1,
-    justifyContent: "center"
+  emojiSelectBoxOpen: {
+    position: 'absolute',
+    width: 300,
+    height: 100,
+    bottom: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: "#B4B4B4",
+    borderRadius: 10,
+    backgroundColor: 'white',
+    elevation: 4,
   },
-  scrollViewBox: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
+  emojiSelectBoxClose: {
+    opacity: 0,
+    position: 'absolute',
+    width: 300,
+    height: 100,
+    bottom: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: "#B4B4B4",
+    borderRadius: 10,
+    backgroundColor: 'white',
+    elevation: 4,
   },
-  scrollViewInner: {
-    height: 40,
-    marginBottom: 50,
+  emojiSelectRow: {
+    flex: 1, 
+    flexDirection: 'row',
+    width: '100%', 
+    height: 10
   },
-  scrollView: {
-    height: 30,
-  },  
-  colorBox: {
-    width: 40,
-    height: 40,
-    borderColor: 'black',
-    borderWidth: 2,
-    marginHorizontal: 15
-  },
-  blankBox: {
-    width: clientWidth/2-65,
-    height: 40,
-    marginHorizontal: 15
+  emojiSelectCol: {
+    flex:1, 
+    width: 10, 
+    height: '100%',
+    padding: 5
   },
 });
 
