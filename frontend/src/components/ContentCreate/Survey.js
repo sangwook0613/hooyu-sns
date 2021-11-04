001942e099199e2d32be5890a64315ba6fc24869
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-11-04 16:41:08 +0900

    S05P31A101-110 Feat: Create Survey Form Layout

--- frontend/src/components/ContentCreate/Survey.js
+++ frontend/src/components/ContentCreate/Survey.js
@@ -1,14 +1,22 @@
 import React, {useRef, useState} from 'react';
-import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions, TextInput, Image } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, KeyboardAvoidingView, ScrollView, Dimensions, TextInput, Image, Button } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import LinearGradient from 'react-native-linear-gradient';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 
 const clientWidth = Dimensions.get('screen').width
 const clientHeight = Dimensions.get('screen').height
 
-const colorArray = ['#FFD0D0', 'red', 'orange', 'yellow', 'green', 'blue', 'purple']
+const emojiArray = [
+  ['amazing', 'amazing', 'amazing', 'amazing', 'amazing', 'amazing'], 
+  ['amazing', 'amazing', 'amazing', 'amazing', 'amazing', 'amazing']
+]
 
 const Survey = ({ navigation, route }) => {
   
+  const [isEmojiSelect, setIsEmojiSelect] = useState(false)
+
   const SurveyTitle = () => {
     return (
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
@@ -36,81 +44,74 @@ const Survey = ({ navigation, route }) => {
   }, [navigation]);
 
 
-  const [color, setColor] = useState('#FFD0D0')
-  const [colorScrollX, setColorScrollX] = useState(0)
-  const statusBackground = useRef()
-  const colorScroll = useRef()
-
-  const onEndScroll = () => {
-    colorScroll.current.scrollTo({ x : parseInt((colorScrollX+35)/70)*70 })
-  }
-
-  const onColorPress = (index) => {
-    colorScroll.current.scrollTo({ x : index*70 })
-    // setIsBlack('white')
-  }
-
-  const backgroundStyle = (color) => {
-    return {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      // backgroundColor: "#FFD0D0",
-      backgroundColor: color,
-    }
-  }
-
   return (
-    <View style={backgroundStyle(color)} ref={statusBackground}>
-      <View style={styles.foo}>
-        <Text></Text>
-      </View>
-      <View style={styles.statusBox}>
-        <TextInput 
-          placeholder={"설문을 입력해주세요"}
-          />
-      </View>
-      <View style={styles.scrollViewBox} >
-        <View style={styles.scrollViewInner}>
-          <ScrollView 
-            style={styles.scrollView}
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            onScroll = {(e) => {
-              setColorScrollX(e.nativeEvent.contentOffset.x)
-              setColor(colorArray[parseInt((colorScrollX+35)/70)])
-            }}
-            onScrollEndDrag={onEndScroll}
-            ref={colorScroll}
-          >
-            <View style={styles.blankBox}></View>
-            {
-              colorArray.map((color, index) => (
-                <TouchableOpacity 
-                onPress={() => {onColorPress(index)}} 
-                key={index}
-                >
-                  <View
-                    style={{ 
-                      width: 40,
-                      height: 40,
-                      backgroundColor: color,
-                      borderColor: colorArray[parseInt((colorScrollX+35)/70)] === color ? 'black':'white' ,
-                      borderWidth: 2,
-                      marginHorizontal: 15
+    <LinearGradient colors={["#AB79EF", "#FC98AB"]} style={styles.mainView}>
+      <TouchableWithoutFeedback onPress={() => {setIsEmojiSelect(false)}} style={{flex: 1, width: clientWidth, alignItems: 'center'}}>
+        <KeyboardAwareScrollView style={{flex: 1}}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.surveyTitleView}>
+            <Text>무엇이 궁금하신가요?</Text>
+            <View style={{width: clientWidth*0.8, height: 40, backgroundColor: 'white', borderRadius: 3, justifyContent: 'center', marginTop: 10, paddingHorizontal: 10}}>
+              <TextInput style={{height: '100%'}}
+                placeholder={"질문을 입력해주세요"}
+              />
+            </View>
+          </View>
+          <View style={styles.surveyContentView}>
+            <Text>옵션</Text>
+            <View style={{width: clientWidth*0.8, height: 40, backgroundColor: 'white', borderRadius: 3, justifyContent: 'center', marginTop: 10, paddingHorizontal: 10}}>
+              <TextInput style={{height: '100%'}}
+                placeholder={"옵션을 입력해주세요"}
+              />
+            </View>
+            <View style={{width: clientWidth*0.8, height: 40, backgroundColor: 'white', borderRadius: 3, justifyContent: 'center', marginTop: 10, paddingHorizontal: 10}}>
+              <TextInput style={{height: '100%'}}
+                placeholder={"옵션을 입력해주세요"}
+              />
+            </View>
+            <View style={{width: clientWidth*0.8, height: 40, backgroundColor: 'white', borderRadius: 3, justifyContent: 'center', marginTop: 10, paddingHorizontal: 10}}>
+              <TextInput style={{height: '100%'}}
+                placeholder={"옵션을 입력해주세요"}
+              />
+            </View>
+            <View style={{width: clientWidth*0.8, height: 40, backgroundColor: 'white', borderRadius: 3, justifyContent: 'center', marginTop: 10, paddingHorizontal: 10}}>
+              <TextInput style={{height: '100%'}}
+                placeholder={"옵션을 입력해주세요"}
+              />
+            </View>
+            <View style={{width: clientWidth*0.8, height: 40, backgroundColor: 'white', borderRadius: 3, justifyContent: 'center', marginTop: 10, paddingHorizontal: 10}}>
+              <TextInput style={{height: '100%'}}
+                placeholder={"옵션을 입력해주세요"}
+              />
+            </View>
+          </View>
+        </KeyboardAwareScrollView>
+      </TouchableWithoutFeedback>
+      { isEmojiSelect && 
+        <View style={styles.emojiSelect}>
+          { [0, 1].map((num, index) => (
+            <View key={index} style={styles.emojiSelectRow}>
+              {emojiArray[num].map((emotion, index2) => (
+                <View key={index2} style={styles.emojiSelectCol}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setIsEmojiSelect(false)
+                      console.warn(index, index2)
                     }}
-                  ></View>
-                </TouchableOpacity>
-                
-              ))
-            }
-            <View style={styles.blankBox}></View>
-
-          </ScrollView>
+                    >
+                    <Image 
+                      source={route.params.emoji}
+                      style={{width: '100%', height: '100%'}}
+                      />
+                  </TouchableOpacity>
+                </View>
+                ))}
+            </View>
+          ))}
         </View>
-      </View>
-    </View>
+      }
+    </LinearGradient>
   )
 }
 
@@ -120,38 +121,39 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'space-between',
-    backgroundColor: "#FFD0D0",
   },
-  foo: {
+  surveyTitleView: {
     flex: 1,
-  },
-  statusBox: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  scrollViewBox: {
-    flex: 1,
-    flexDirection: 'column',
     justifyContent: 'flex-end',
+    padding: 5,
+    marginTop: 80
   },
-  scrollViewInner: {
-    height: 40,
-    marginBottom: 50,
+  surveyContentView: {
+    flex: 2,
+    padding: 5,
+    paddingTop: 30,
   },
-  scrollView: {
-    height: 30,
-  },  
-  colorBox: {
-    width: 40,
-    height: 40,
-    borderColor: 'black',
-    borderWidth: 2,
-    marginHorizontal: 15
+  emojiSelect: {
+    position: 'absolute',
+    width: 300,
+    height: 100,
+    borderWidth: 1,
+    borderColor: "#B4B4B4",
+    borderRadius: 10,
+    backgroundColor: 'white',
+    elevation: 4,
+    },
+  emojiSelectRow: {
+    flex: 1, 
+    flexDirection: 'row',
+    width: '100%', 
+    height: 10
   },
-  blankBox: {
-    width: clientWidth/2-65,
-    height: 40,
-    marginHorizontal: 15
+  emojiSelectCol: {
+    flex:1, 
+    width: 10, 
+    height: '100%',
+    padding: 5
   },
 });
 
