b9e4087b15eed7069da08a63bc5abec025d407ee
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-11-04 15:21:28 +0900

    S05P31A101-111 Feat: Create Picture Upload page

--- frontend/src/components/ContentCreate/Picture.js
+++ frontend/src/components/ContentCreate/Picture.js
@@ -1,14 +1,23 @@
 import React, {useRef, useState} from 'react';
-import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions, TextInput, Image } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions, TextInput, Image, Button } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import LinearGradient from 'react-native-linear-gradient';
+import imageUpload from '../../assets/createcontent/uploadImage.png'
+import * as ImagePicker from 'react-native-image-picker';
 
 
 const clientWidth = Dimensions.get('screen').width
 const clientHeight = Dimensions.get('screen').height
 
-const colorArray = ['#FFD0D0', 'red', 'orange', 'yellow', 'green', 'blue', 'purple']
+const emojiArray = [
+  ['amazing', 'amazing', 'amazing', 'amazing', 'amazing', 'amazing'], 
+  ['amazing', 'amazing', 'amazing', 'amazing', 'amazing', 'amazing']
+]
 
 const Picture = ({ navigation, route }) => {
   
+  const [isEmojiSelect, setIsEmojiSelect] = useState(false)
+
   const PictureTitle = () => {
     return (
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
@@ -35,82 +44,107 @@ const Picture = ({ navigation, route }) => {
     });
   }, [navigation]);
 
+  const [imageFile, setImageFile] = useState(null)
 
-  const [color, setColor] = useState('#FFD0D0')
-  const [colorScrollX, setColorScrollX] = useState(0)
-  const statusBackground = useRef()
-  const colorScroll = useRef()
+  const imageGalleryLaunch = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+  
+    ImagePicker.launchImageLibrary(options, (res) => {
+      console.log('Response = ', res);
+  
+      if (res.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (res.error) {
+        console.log('ImagePicker Error: ', res.error);
+      } else if (res.customButton) {
+        console.log('User tapped custom button: ', res.customButton);
+        alert(res.customButton);
+      } else {
+        const source = { uri: res.assets[0].uri };
+        // console.log('response', JSON.stringify(res));
+        console.log(res.assets[0].uri)
+        setImageFile(source)
+      }
+    });
+  }  
 
-  const onEndScroll = () => {
-    colorScroll.current.scrollTo({ x : parseInt((colorScrollX+35)/70)*70 })
-  }
-
-  const onColorPress = (index) => {
-    colorScroll.current.scrollTo({ x : index*70 })
-    // setIsBlack('white')
-  }
-
-  const backgroundStyle = (color) => {
-    return {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      // backgroundColor: "#FFD0D0",
-      backgroundColor: color,
-    }
-  }
 
   return (
-    <View style={backgroundStyle(color)} ref={statusBackground}>
-      <View style={styles.foo}>
-        <Text></Text>
-      </View>
-      <View style={styles.statusBox}>
-        <TextInput 
-          placeholder={"사진을 입력해주세요"}
-          />
-      </View>
-      <View style={styles.scrollViewBox} >
-        <View style={styles.scrollViewInner}>
-          <ScrollView 
-            style={styles.scrollView}
-            showsHorizontalScrollIndicator={false}
-            horizontal={true}
-            onScroll = {(e) => {
-              setColorScrollX(e.nativeEvent.contentOffset.x)
-              setColor(colorArray[parseInt((colorScrollX+35)/70)])
-            }}
-            onScrollEndDrag={onEndScroll}
-            ref={colorScroll}
-          >
-            <View style={styles.blankBox}></View>
-            {
-              colorArray.map((color, index) => (
-                <TouchableOpacity 
-                onPress={() => {onColorPress(index)}} 
-                key={index}
+    <LinearGradient colors={["#AB79EF", "#FC98AB"]} style={styles.mainView}>
+      <TouchableWithoutFeedback onPress={() => {setIsEmojiSelect(false)}} style={{flex: 1}}>
+        <View style={{width: clientWidth, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          {
+            imageFile ? 
+            <View>
+              <View style={{ elevation: 15}}>
+                <Image
+                  style={{ width: clientWidth-80, height: clientWidth-80 }}
+                  source={imageFile}
+                />
+              </View>
+              <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center', marginTop: 50}} onPress={() => imageGalleryLaunch()}>
+                <View 
+                  style={{ 
+                    // borderWidth: 1,
+                    borderColor: 'red',
+                    elevation: 4,
+                    width: 200, 
+                    height: 40, 
+                    justifyContent: 'center', 
+                    alignItems: 'center',
+                    borderRadius: 10,
+                    backgroundColor: 'crimson'
+                  }}
                 >
-                  <View
-                    style={{ 
-                      width: 40,
-                      height: 40,
-                      backgroundColor: color,
-                      borderColor: colorArray[parseInt((colorScrollX+35)/70)] === color ? 'black':'white' ,
-                      borderWidth: 2,
-                      marginHorizontal: 15
-                    }}
-                  ></View>
-                </TouchableOpacity>
-                
-              ))
-            }
-            <View style={styles.blankBox}></View>
-
-          </ScrollView>
+                  <Text style={{ textAlign: 'center', color: 'white' }}>Select Another Photo</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            : 
+            <TouchableOpacity onPress={() => imageGalleryLaunch()}>
+              <Image
+                style={{ width: 150, height: 150, marginLeft: 25 }}
+                source={imageUpload}
+              />
+            </TouchableOpacity>
+          }
+          {/* <TouchableOpacity onPress={() => imageGalleryLaunch()}>
+            <Image
+              style={{ width: 150, height: 150, marginLeft: 25 }}
+              source={imageUpload}
+            />
+          </TouchableOpacity> */}
         </View>
-      </View>
-    </View>
+      </TouchableWithoutFeedback>
+      { isEmojiSelect && 
+        <View style={styles.emojiSelect}>
+          { [0, 1].map((num, index) => (
+            <View key={index} style={styles.emojiSelectRow}>
+              {emojiArray[num].map((emotion, index2) => (
+                <View key={index2} style={styles.emojiSelectCol}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setIsEmojiSelect(false)
+                      console.warn(index, index2)
+                    }}
+                    >
+                    <Image 
+                      source={route.params.emoji}
+                      style={{width: '100%', height: '100%'}}
+                      />
+                  </TouchableOpacity>
+                </View>
+                ))}
+            </View>
+          ))}
+        </View>
+      }
+    </LinearGradient>
   )
 }
 
@@ -120,38 +154,28 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'space-between',
-    backgroundColor: "#FFD0D0",
   },
-  foo: {
-    flex: 1,
+  emojiSelect: {
+    position: 'absolute',
+    width: 300,
+    height: 100,
+    borderWidth: 1,
+    borderColor: "#B4B4B4",
+    borderRadius: 10,
+    backgroundColor: 'white',
+    elevation: 4,
+    },
+  emojiSelectRow: {
+    flex: 1, 
+    flexDirection: 'row',
+    width: '100%', 
+    height: 10
   },
-  statusBox: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  scrollViewBox: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-  },
-  scrollViewInner: {
-    height: 40,
-    marginBottom: 50,
-  },
-  scrollView: {
-    height: 30,
-  },  
-  colorBox: {
-    width: 40,
-    height: 40,
-    borderColor: 'black',
-    borderWidth: 2,
-    marginHorizontal: 15
-  },
-  blankBox: {
-    width: clientWidth/2-65,
-    height: 40,
-    marginHorizontal: 15
+  emojiSelectCol: {
+    flex:1, 
+    width: 10, 
+    height: '100%',
+    padding: 5
   },
 });
 
