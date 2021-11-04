7713e302817a539f3e509df0e4d0b682b09ce2a2
dawit95 <dawit0310@gmail.com>
2021-11-04 18:15:17 +0900

    S05P31A101-85 feat : Add user content recentlyTime

--- backend/src/main/java/com/status/backend/content/dto/RequestContentTimeDto.java
+++ backend/src/main/java/com/status/backend/content/dto/RequestContentTimeDto.java
@@ -1,5 +1,6 @@
 package com.status.backend.content.dto;
 
+import com.status.backend.content.domain.RecordTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,4 +17,11 @@ public class RequestContentTimeDto {
     private LocalDateTime status;
     private LocalDateTime images;
     private LocalDateTime survey;
+
+    public RequestContentTimeDto(RecordTime entity) {
+        this.status = entity.getStatusAt();
+        this.images = entity.getImageAt();
+        this.survey = entity.getSurveyAt();
+    }
+
 }
