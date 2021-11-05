b1da7410b116c190343f9a23dc273196ec25cd64
ryu <munam4@naver.com>
2021-11-06 02:18:35 +0900

    S05P31A101-102 feat: Add a login axios feature, interceptor file

---
+++ whoyouapp/src/utlis/api.js
@@ -0,0 +1,20 @@
+import axios from './interceptor'
+
+export default {
+
+  loginGoogle(data) {
+    return axios({
+      url: 'login/google',
+      method: 'post',
+      data: { googleIdToken: data }
+    })
+  },
+
+  getUser(pk) {
+    return axios({
+      url: `user/setup/${pk}`,
+      method: 'get'
+    })
+  },
+
+}
