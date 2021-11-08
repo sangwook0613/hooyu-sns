2da075585096cce8f9fb65c3fa12d59e57d0206a
sangwook0613 <kvmoke@gmail.com>
2021-11-09 00:26:03 +0900

    S05P31A101-130 feat: setup pushSetting & privatezoneSetting layout

--- whoyouapp/src/screen/Setting/PushSetting.js
+++ whoyouapp/src/screen/Setting/PushSetting.js
@@ -1,16 +1,97 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, Dimensions, Switch } from 'react-native'
+import PushButtonGroup from './PushButtonGroup'
 
-const PushSetting = (props) => {
-  const [isModalVisible, setModalVisible] = useState(false)
+const deviceWidth = Dimensions.get('window').width
+const deviceHeight = Dimensions.get('window').height
+
+const PushSetting = () => {
+  const [isPushEnabled, setIsPushEnabled] = useState(false)
+  const [isPushSyncEnabled, setIsPushSyncEnabled] = useState(false)
+  const pushToggleSwitch = () => {
+    setIsPushEnabled(!isPushEnabled)
+    setIsPushSyncEnabled(false)
+  }
+  const pushSyncToggleSwitch = () => setIsPushSyncEnabled(!isPushSyncEnabled)
+
 
   return (
     <>
-      <TouchableOpacity>
-        <View><Text>푸시 알림 받기</Text></View>
-      </TouchableOpacity>
-     <View><Text>푸시 알림 반경 메인과 동기화</Text></View>
-     <View><Text>푸시 알람 반경</Text></View>
+      <View
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: 'white',
+          paddingLeft: 20,
+          paddingRight: 20,
+          borderBottomWidth: 1,
+          borderBottomColor: '#E5E5E5',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ fontSize: 16, fontWeight: '700' }}>푸시 알림 받기</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#F38181" }}
+          thumbColor={isPushEnabled ? "white" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={pushToggleSwitch}
+          value={isPushEnabled}
+        />
+      </View>
+      <View
+        pointerEvents={isPushEnabled ? "auto" : "none"} 
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: `${isPushEnabled ? 'white' : "#E5E5E5"}`,
+          paddingLeft: 20,
+          paddingRight: 20,
+          borderBottomWidth: 1,
+          borderBottomColor: '#E5E5E5',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: `${isPushEnabled ? 'black' : "#767577"}`, fontSize: 16, fontWeight: '700' }}>푸시 알림 반경 메인과 동기화</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#F38181" }}
+          thumbColor={isPushSyncEnabled ? "white" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={pushSyncToggleSwitch}
+          value={isPushSyncEnabled}
+        />
+      </View>
+      <View
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: 'white',
+          paddingLeft: 20,
+          paddingRight: 20,
+          justifyContent: 'center',
+          backgroundColor: `${isPushSyncEnabled ? 'white' : "#E5E5E5"}`,
+        }}
+      >
+        <Text style={{ color: `${isPushSyncEnabled ? 'black' : "#767577"}`, fontSize: 16, fontWeight: '700' }}>푸시 알람 반경</Text>
+      </View>
+      <View
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: 'white',
+          paddingLeft: 20,
+          paddingRight: 20,
+          justifyContent: 'center',
+          borderBottomWidth: 1,
+          borderBottomColor: '#E5E5E5',
+          backgroundColor: `${isPushSyncEnabled ? 'white' : "#E5E5E5"}`,
+        }}
+      >
+        <PushButtonGroup isPushSyncEnabled={isPushSyncEnabled}/>
+      </View>
     </>
   )
 }
--- whoyouapp/src/screen/SettingScreen.jsx
+++ whoyouapp/src/screen/SettingScreen.jsx
@@ -1,18 +1,67 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'; 
+
+const deviceWidth = Dimensions.get('window').width
+const deviceHeight = Dimensions.get('window').height
+
 
 const SettingScreen = ({ navigation: { navigate }}) => {
   return (
     <View>
-      <Text>Setting Screen</Text>
-      <TouchableOpacity onPress={() => navigate("UserSetting")}>
-        <Text>계정 설정</Text>
+      <TouchableOpacity
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: 'white',
+          paddingLeft: 20,
+          paddingRight: 20,
+          borderBottomWidth: 1,
+          borderBottomColor: '#E5E5E5',
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+        onPress={() => navigate("UserSetting")}
+      >
+        <MaterialIcons name="person" size={24} color="black" />
+        <Text style={{marginLeft: 16, fontSize: 16, fontWeight: '700'}}>계정 설정</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("PrivateZoneSetting")}>
-        <Text>프라이빗 존</Text>
+      <TouchableOpacity 
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: 'white',
+          paddingLeft: 20,
+          paddingRight: 20,
+          borderBottomWidth: 1,
+          borderBottomColor: '#E5E5E5',
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+        onPress={() => navigate("PrivateZoneSetting")}
+      >
+        <MaterialIcons name="lock" size={24} color="black" />
+        <Text style={{marginLeft: 16, fontSize: 16, fontWeight: '700'}}>프라이빗 존</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigate("PushSetting")}>
-        <Text>푸시 알림</Text>
+      <TouchableOpacity 
+        style={{
+          width: deviceWidth,
+          height: 60,
+          backgroundColor: 'white',
+          paddingLeft: 20,
+          paddingRight: 20,
+          borderBottomWidth: 1,
+          borderBottomColor: '#E5E5E5',
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+        onPress={() => navigate("PushSetting")}
+      >
+        <MaterialCommunityIcons name="bell-ring" size={24} color="black" />
+        <Text style={{marginLeft: 16, fontSize: 16, fontWeight: '700'}}>푸시 알림</Text>
       </TouchableOpacity>
     </View>
   )
