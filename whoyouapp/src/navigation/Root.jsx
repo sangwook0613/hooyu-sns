8bc424a76abe85d7dc90d485660a82df9a3fb596
ryu <munam4@naver.com>
2021-11-08 21:31:09 +0900

    S05P31A101-126 feat : Add a navigating start screen feature

--- whoyouapp/src/navigation/Root.jsx
+++ whoyouapp/src/navigation/Root.jsx
@@ -14,28 +14,43 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { connect } from 'react-redux'
 import { actionCreators } from '../store/reducers'
 import jwt_decode from "jwt-decode";
+import api from '../utils/api'
+// import axios from 'axios'
+
 
 const Nav = createNativeStackNavigator()
 
-const Root = ({ setUserPK }) => {
+const Root = ({ setUserPK, setUserEmoji, userEmoji }) => {
   const navigation = useNavigation();
 
   const [accessToken, setAccessToken] = useState(null)
-  const [isReady, setIsReady] = useState(false);
+  const [isReady, setIsReady] = useState(false)
+  const [emoji, setEmoji] = useState(null)
 
   useEffect(() => {
-    AsyncStorage.getItem('access_token', (err, result) => {
+    AsyncStorage.getItem('access_token', async (err, result) => {
       if (result) {
+        console.log('result :', result)
+        console.log('pk :', jwt_decode(result).pk)
         setUserPK(jwt_decode(result).pk)
         setAccessToken(result)
+
+        await api.getUser(jwt_decode(result).pk)
+          .then((res) => {
+            console.log(res.data.success.emoji)
+            setUserEmoji(res.data.success.emoji)
+            setEmoji(res.data.success.emoji)
+          }).catch((err) => {
+            console.log(err)
+          })
       }
-      setTimeout(() => {
-        setIsReady(true)
-      }, 5000)
+
+      setIsReady(true)
+
     })
 
-  },[])
-  
+  }, [])
+
   if (!isReady) {
     return (
       <>
@@ -45,7 +60,7 @@ const Root = ({ setUserPK }) => {
 
   return (
     <Nav.Navigator
-      initialRouteName={accessToken ? "Main" : "Login"}
+      initialRouteName={(accessToken && emoji) ? "Main" : ((accessToken) ? "NicknameTutorial" : "Login")}
       screenOptions={{
         headerShown: false,
         animation: 'slide_from_right',
@@ -62,7 +77,7 @@ const Root = ({ setUserPK }) => {
       <Nav.Screen name="Profile" component={ProfileScreen}
         options={{
           headerShown: true,
-        }}/>
+        }} />
       <Nav.Screen name="Setting" component={SettingScreen} options={{
         headerShown: true,
         headerTitle: "설정",
@@ -72,7 +87,7 @@ const Root = ({ setUserPK }) => {
       <Nav.Screen name="User" component={UserScreen}
         options={{
           headerShown: true,
-        }}/>
+        }} />
       {/* 세부 설정 */}
       {/* <Nav.Screen name="UserSetting" component={SettingScreen} options={{
         headerShown: true,
@@ -86,7 +101,7 @@ const Root = ({ setUserPK }) => {
         headerShown: true,
         headerTitle: "푸시 알림",
       }} /> */}
-      
+
       {/* 컨텐츠 생성 */}
       <Nav.Screen name="CreateContent" component={CreateContent} options={{ headerShown: true }} />
       {/* <Nav.Screen name="CreateStatus" component={Status} options={{ headerShown: true }} /> */}
@@ -97,12 +112,22 @@ const Root = ({ setUserPK }) => {
   )
 }
 
+function mapStateToProps(state) {
+  return {
+    userPK: state.user.userPK,
+    userEmoji: state.user.userEmoji,
+  }
+}
+
 function mapDispatchToProps(dispatch) {
   return {
     setUserPK: (pk) => {
       dispatch(actionCreators.setUserPK(pk))
+    },
+    setUserEmoji: (emoji) => {
+      dispatch(actionCreators.setUserEmoji(emoji))
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(Root)
+export default connect(mapStateToProps, mapDispatchToProps)(Root)
