90493810dfdc5f8defaaf0f36cf8b0839f1de403
ryu <munam4@naver.com>
2021-11-08 13:20:25 +0900

    feat : Add a login redux

--- whoyouapp/src/store/reducers.js
+++ whoyouapp/src/store/reducers.js
@@ -9,20 +9,31 @@ const defaultState = {
   deviceHeight: Dimensions.get('window').height,
   myRadius: 2000,
   SERVER_URL: 'https://k5a101.p.ssafy.io/api/v1/',
-  userPK: 2,
+  userPK: 0,
+  userEmoji: 'none',
 }
 
 // Actions 정의
 const login = createAction("LOGIN")
 const logout = createAction("LOGOUT")
 const setRadius = createAction("SETRADIUS")
+const setUserPK = createAction("SETUSERPK")
+const setUserEmoji = createAction("SETUSEREMOJI")
 
 // Reducer 정의
 const UserReducer = createReducer(defaultState, {
-  [login]: (state, action) => {
+  [setUserPK]: (state, action) => ({
+    ...state,
+    userPK: action.payload
     // 로그인 시 state 변경 로직
     // state.push({ text: action.payload, id: Date.now() });
-  },
+  }),
+  [setUserEmoji]: (state, action) => ({
+    ...state,
+    userEmoji: action.payload
+    // 로그인 시 state 변경 로직
+    // state.push({ text: action.payload, id: Date.now() });
+  }),
   [setRadius]: (state, action) => ({
     ...state,
     myRadius: action.payload
@@ -30,7 +41,8 @@ const UserReducer = createReducer(defaultState, {
 });
 
 export const actionCreators = {
-  login,
+  setUserPK,
+  setUserEmoji,
   logout,
   setRadius
 }
