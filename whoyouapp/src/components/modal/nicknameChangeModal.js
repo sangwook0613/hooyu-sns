d4c9ce2181fd66a8e92f69b9d1a0cb10085e2507
ty1295 <ty1295@naver.com>
2021-11-10 00:47:28 +0900

    S05P31A101-131 feat: SettingPage userSetting & pushSetting axios connect

--- whoyouapp/src/components/modal/nicknameChangeModal.js
+++ whoyouapp/src/components/modal/nicknameChangeModal.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
-import Modal from "react-native-modal";
+import React, { useState } from 'react'
+import { Text, View, TouchableOpacity, TextInput } from 'react-native'
+import Modal from "react-native-modal"
+import Api from '../../utils/api'
+import { connect } from 'react-redux'
 
-const NicknameChangeModal = ({ isModalVisible, setModalVisible }) => {  
+const NicknameChangeModal = ({ isModalVisible, setModalVisible, userPK  }) => {  
+  const [newNickname, setNewNickname] = useState('')
+
   const sendModalVisible = () => {
+    setNewNickname('')
     setModalVisible(!isModalVisible)
   }
 
   const sendReport = () => {
-    console.warn('Change Nickname')
+    registerNickname()
     sendModalVisible()
   }
+
+  const registerNickname = () => {
+    Api.isDuplicatedNickname(newNickname)
+    .then((res) => {
+      if (res.data.success !== 'Success') {
+        alert('중복된 닉네임입니다.')
+      } else {
+        changeNickname()
+      }
+    })
+    .catch((err) => {
+      console.warn(err)
+    })
+  }
+  
+  const changeNickname = () => {
+    Api.updateNickname(newNickname, userPK)
+      .then((res) => {
+        console.log(res.data.success)
+      })
+      .catch((err) => {
+        console.warn(err)
+      })
+  }
+  
 
   return (
     <Modal 
@@ -29,24 +59,32 @@ const NicknameChangeModal = ({ isModalVisible, setModalVisible }) => {
       }}>
         <Text style={{fontSize: 22, fontWeight: 'bold', marginBottom: 20}}>닉네임 변경</Text>
         <Text style={{fontSize: 14, marginBottom: 2}}>변경할 닉네임을 입력해주세요.</Text>
-        <Text style={{fontSize: 14, marginBottom: 20, color: 'gray'}}>(한글, 영어 포함 8글자)</Text>
+        <Text style={{fontSize: 14, marginBottom: 20, color: 'gray'}}>(한글 혹은 영어 포함 최대 10글자)</Text>
         <TextInput
-          style={{ borderBottomWidth: 2, paddingBottom: 4 }}
+          style={{ borderBottomWidth: 2, paddingBottom: 4, fontSize: 16, }}
           placeholder={"옵션을 입력해주세요"}
-          // onChangeText={(text) => onTextChange(1, text)}
+          autoCapitalize={'none'}
+          value={newNickname}
+          maxLength={10}
+          onChangeText={(text) => setNewNickname(text)}
         />
         <View style={{paddingTop: 30, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
           <TouchableOpacity style={{paddingRight: 30}} onPress={sendModalVisible}>
             <Text style={{fontSize: 16, color: 'black'}}>취소</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{paddingRight: 20}} onPress={sendReport}>
+          <TouchableOpacity style={{paddingRight: 20}} disabled={newNickname === ''} onPress={sendReport}>
             <Text style={{fontSize: 16, color: 'red'}}>네</Text>
           </TouchableOpacity>
         </View>
       </View>
     </Modal>
   )
-  
-};
+}
 
-export default NicknameChangeModal;
+function mapStateToProps(state) {
+  return {
+    userPK: state.user.userPK,
+  }
+}
+
+export default connect(mapStateToProps)(NicknameChangeModal)
