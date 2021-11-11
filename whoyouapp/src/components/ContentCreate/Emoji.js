ae12a5221e8c616e7c9c60c4000e3979f84f08ab
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-11-11 16:16:20 +0900

    S05P31A101-137 Feat: Emoji choose Animation

--- whoyouapp/src/components/ContentCreate/Emoji.js
+++ whoyouapp/src/components/ContentCreate/Emoji.js
@@ -1,22 +1,49 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { Text, TouchableOpacity, View, StyleSheet, ScrollView, Dimensions, TextInput, Image } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, ScrollView, TouchableWithoutFeedback, Dimensions, TextInput, Image, Animated } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import axios from 'axios'
 import { connect } from 'react-redux'
 import { actionCreators } from '../../store/reducers'
 import * as emojiImages from '../../assets/images'
+import { AntDesign } from "@expo/vector-icons"
+import { map } from 'lodash';
 
+
+const clientWidth = Dimensions.get('screen').width
+const clientHeight = Dimensions.get('screen').height
+const radius = clientWidth * 1/3
+const emojiMoveLeft = [0, 0, radius*1/2, radius*Math.sqrt(3)/2, radius, radius*Math.sqrt(3)/2, radius*1/2, 0, -radius*1/2, -radius*Math.sqrt(3)/2, -radius, -radius*Math.sqrt(3)/2, -radius*1/2 ]
+const emojiMoveTop = [0, -radius, -radius*Math.sqrt(3)/2, -radius*1/2, 0, radius*1/2, radius*Math.sqrt(3)/2, radius, radius*Math.sqrt(3)/2, radius*1/2, 0, -radius*1/2, -radius*Math.sqrt(3)/2 ]
 
 const emojiArray = [
-  ['smile', 'amazing', 'sad', 'crying', 'sense', 'angry'], 
-  ['pouting', 'pokerface', 'love', 'sunglass', 'hard', 'sleep']
+  'smile', 'amazing', 'sad', 'crying', 'sense', 'angry', 'pouting', 'pokerface', 'love', 'sunglass', 'hard', 'sleep'
 ]
 
-const Emoji = ({ navigation, route, setUserEmoji, SERVER_URL, userPK, userEmoji }) => {
+const Emoji = ({ navigation, setUserEmoji, SERVER_URL, userPK, userEmoji }) => {
   
-  const [isEmojiSelect, setIsEmojiSelect] = useState(false)
+  const open = useRef(new Animated.Value(0)).current
   const [emoji, setEmoji] = useState(userEmoji)
-  const [checked, setChecked] = useState(false)
+  const [isEmojiSelect, setIsEmojiSelect] = useState(false)
+
+  const range = (n) => {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+      arr.push(i/n)
+    }
+    arr.push(1)
+    return arr
+  }
+
+  const toggleMenu = () => {
+    Animated.timing(open, {
+      toValue: isEmojiSelect ? 0 : 1,
+      duration: 800,
+      useNativeDriver: false,
+    }).start()
+
+    setIsEmojiSelect(!isEmojiSelect)
+  }
+
 
   const EmojiTitle = () => {
     return (
@@ -30,15 +57,16 @@ const Emoji = ({ navigation, route, setUserEmoji, SERVER_URL, userPK, userEmoji 
     navigation.setOptions({
       headerTitle: (props) => <EmojiTitle {...props} />,
       headerRight: () => (
-        <TouchableOpacity style={{ marginRight: 10 }} >
-          <Text></Text>
+        <TouchableOpacity style={{ marginRight: 10 }} 
+          onPress={() => {
+            createEmoji()
+            navigation.navigate('Main')
+          }}
+        >
+          <Text>게시</Text>
         </TouchableOpacity>
       )
     });
-    if (checked) {
-      createEmoji()
-      navigation.navigate('Main')
-    }
   }, [navigation, emoji]);
 
   const createEmoji = () => {
@@ -64,36 +92,60 @@ const Emoji = ({ navigation, route, setUserEmoji, SERVER_URL, userPK, userEmoji 
   return (
     <LinearGradient colors={["#AB79EF", "#FC98AB"]} style={styles.mainView}>
       <View style={styles.emojiSelect}>
-        <TouchableOpacity
-          onPress={() => {setIsEmojiSelect(!isEmojiSelect)}}
-        >
-          <Image
-            style={{ width: 50, height: 50 }}
-            source={emojiImages.default.emoji[userEmoji]}
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={isEmojiSelect ? styles.emojiSelectBoxClose: styles.emojiSelectBoxOpen}>
-        { [0, 1].map((num, index) => (
-          <View key={index} style={styles.emojiSelectRow}>
-            {emojiArray[num].map((emotion, index2) => (
-              <View key={index2} style={styles.emojiSelectCol}>
-                <TouchableOpacity
-                  onPress={() => {
-                    setIsEmojiSelect(false)
-                    setEmoji(emojiArray[index][index2])
-                    setChecked(true)
-                  }}
-                  >
-                  <Image 
-                    source={emojiImages.default.emoji[emotion]}
-                    style={{width: '100%', height: '100%'}}
-                    />
-                </TouchableOpacity>
-              </View>
-              ))}
-          </View>
-        ))}
+        {
+          emojiArray.map((emotion, index) => (
+            <Animated.View key={index} style={['', {
+              left: open.interpolate({
+                inputRange: range(index+1),
+                outputRange: emojiMoveLeft.slice(0, index+2)
+              }),
+              top: open.interpolate({
+                inputRange: range(index+1),
+                outputRange: emojiMoveTop.slice(0, index+2)
+              })
+            }]}>
+              <TouchableOpacity
+                style={styles.selectableEmoji}
+                onPress={() => {
+                  setIsEmojiSelect(false)
+                  toggleMenu()
+                  setEmoji(emojiArray[index])
+                }}
+              >
+                <Image
+                  source={emojiImages.default.emoji[emotion]}
+                  style={{ width: 50, height: 50 }}
+                />
+              </TouchableOpacity>
+            </Animated.View>
+          ))
+        }
+        
+        {
+          isEmojiSelect ?
+          <TouchableOpacity
+          style={styles.disableEmojiSelect}
+          onPress={() => {
+            setIsEmojiSelect(false)
+            toggleMenu()
+          }}
+          >
+            <AntDesign name="close" size={30} color="#b4b4b4" />
+          </TouchableOpacity>
+          :
+          <TouchableOpacity
+          onPress={() => {
+            setIsEmojiSelect(true)
+            toggleMenu()
+          }}
+          style={styles.myEmoji}
+          >
+            <Image
+              source={emojiImages.default.emoji[emoji]}
+              style={{ width: 80, height: 80 }}
+              />
+          </TouchableOpacity>
+        }
       </View>
     </LinearGradient>
   )
@@ -107,50 +159,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   emojiSelect: {
-    width: 200,
-    height: 200,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  emojiSelectBoxOpen: {
-    position: 'absolute',
-    width: 300,
-    height: 100,
-    bottom: 100,
+    flex: 1,
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: "#B4B4B4",
-    borderRadius: 10,
-    backgroundColor: 'white',
-    elevation: 4,
+    // borderWidth: 2
   },
-  emojiSelectBoxClose: {
-    opacity: 0,
+  myEmoji: {
+    width: 80, 
+    height: 80, 
+    borderRadius: 40,
     position: 'absolute',
-    width: 300,
-    height: 100,
-    bottom: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: "#B4B4B4",
-    borderRadius: 10,
-    backgroundColor: 'white',
+    elevation: 6,
+  },
+  disableEmojiSelect: {
+    width: 60, 
+    height: 60, 
+    backgroundColor: '#eee', 
+    borderRadius: 30, 
     elevation: 4,
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    position: 'absolute'
   },
-  emojiSelectRow: {
-    flex: 1, 
-    flexDirection: 'row',
-    width: '100%', 
-    height: 10
-  },
-  emojiSelectCol: {
-    flex:1, 
-    width: 10, 
-    height: '100%',
-    padding: 5
-  },
+  selectableEmoji: { 
+    position: 'absolute',
+    width: 50, 
+    height: 50, 
+    left: -25,
+    top: -25,
+    elevation: 5,
+  }
 });
 
 
