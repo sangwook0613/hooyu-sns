3da340767e1a51e5db1708ee31c63ceaadb6c79c
DESKTOP-KVCQHCD\multicampus <seankim95@naver.com>
2021-11-12 11:55:09 +0900

    S05P31A101-164 Feat: Create Private Zone setting layout

---
+++ whoyouapp/src/components/PrivateZone/SettingPrivateZone.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useRef, useState} from 'react';
+import { Text, TouchableOpacity, View, StyleSheet, ScrollView, TouchableWithoutFeedback, Dimensions, TextInput, Image, Animated } from 'react-native';
+import axios from 'axios'
+import { connect } from 'react-redux'
+
+
+const clientWidth = Dimensions.get('screen').width
+const clientHeight = Dimensions.get('screen').height
+
+const SettingPrivateZone = ({ navigation, userPK, setPrivateZone }) => {
+  
+  const confirmPrivateZone = () => {
+    setPrivateZone('zone')
+  }
+
+  return (
+    <View style={{flex:1}}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{paddingBottom: 70, fontSize: 16}}>프라이빗 존을 설정중입니다.</Text>
+      </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity
+          style={styles.mainButton}
+          onPress={confirmPrivateZone}
+        >
+          <Text style={{fontSize: 16, fontWeight: '700', color: 'white'}}>확인</Text>
+        </TouchableOpacity>
+      </View> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainButton: {
+    width: clientWidth * 0.7,
+    height: 50,
+    backgroundColor: '#F38181',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+
+function mapStateToProps(state) {
+  return {
+    SERVER_URL: state.user.SERVER_URL,
+    userPK: state.user.userPK,
+  }
+}
+
+
+export default connect(mapStateToProps)(SettingPrivateZone);
