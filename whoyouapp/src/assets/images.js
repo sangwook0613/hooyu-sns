e4060c0dc9db4be8c4b6a8a0ee413bcce7dc20bb
qlfflwls5 <qlfflwls2@gmail.com>
2021-11-16 13:42:09 +0900

    S05P31A101-192 feat: Check frontend detail & modify design step 3

--- whoyouapp/src/assets/images.js
+++ whoyouapp/src/assets/images.js
@@ -1,14 +1,14 @@
 const images = {
   emoji: {
     amazing: require('./emoji/amazing.png'),
-    angry: require('./emoji/angry2.png'),
+    angry: require('./emoji/angry.png'),
     crying: require('./emoji/crying.png'),
     hard: require('./emoji/hard.png'),
     like: require('./emoji/like.png'),
     love: require('./emoji/love.png'),
     pokerface: require('./emoji/pokerface.png'),
     pouting: require('./emoji/pouting.png'),
-    sad: require('./emoji/sad2.png'),
+    sad: require('./emoji/sad.png'),
     sense: require('./emoji/sense.png'),
     sleep: require('./emoji/sleep.png'),
     smile: require('./emoji/smile.png'),
