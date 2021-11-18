9fee69ea1b679291471442a9ceca495ca89dead5
dawit95 <dawit0310@gmail.com>
2021-11-18 14:16:51 +0900

    S05P31A101-196 test : check user location dist

--- server/src/main/java/com/status/server/global/dto/DistDto.java
+++ server/src/main/java/com/status/server/global/dto/DistDto.java
@@ -2,8 +2,10 @@ package com.status.server.global.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class DistDto {
     private double xDist;
     private double yDist;
