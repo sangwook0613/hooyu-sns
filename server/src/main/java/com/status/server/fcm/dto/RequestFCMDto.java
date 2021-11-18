8eb69108fd3266708d18fab1de7157fac24ae469
dawit95 <dawit0310@gmail.com>
2021-11-19 01:10:58 +0900

    test : Add RequestFCMDto NoArgsConstructor

--- server/src/main/java/com/status/server/fcm/dto/RequestFCMDto.java
+++ server/src/main/java/com/status/server/fcm/dto/RequestFCMDto.java
@@ -3,9 +3,11 @@ package com.status.server.fcm.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class RequestFCMDto {
 
