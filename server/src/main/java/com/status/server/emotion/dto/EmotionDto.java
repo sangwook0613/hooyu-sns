fcb5d1ce5266df6ebdc9db5def48ac213ee54203
dawit95 <dawit0310@gmail.com>
2021-11-19 00:10:11 +0900

    S05P31A101-201 refactor : Add emotion userPK

--- server/src/main/java/com/status/server/emotion/dto/EmotionDto.java
+++ server/src/main/java/com/status/server/emotion/dto/EmotionDto.java
@@ -9,12 +9,12 @@ import lombok.*;
 @Setter
 @ToString
 public class EmotionDto {
-//    private Long userPK;
+    private Long userPK;
     private Long contentPk;
     private String contentEmoji;
 
     public EmotionDto(Emotion entity) {
-//        this.userPK = entity.getUser().getId();
+        this.userPK = entity.getUser().getId();
         this.contentPk = entity.getContent().getId();
         this.contentEmoji = entity.getContentEmoji();
     }
